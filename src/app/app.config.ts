0189cfcade1bcdc3595d5bfe52fbbedb7ed73b18
Masoud Bimmar <masoudbimar@gmail.com>
2026-02-17 13:14:44 +0100

    Add @ngrx/router-store to the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-// import { provideEffects } from '@ngrx/effects';
+import { provideEffects } from '@ngrx/effects';
 import { provideStore } from '@ngrx/store';
 import { routes } from './app.routes';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideRouterStore } from '@ngrx/router-store';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withComponentInputBinding()),
     provideStore(),
-    // provideEffects(),
-    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
-  ]
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+    provideRouterStore()
+]
 };
+
