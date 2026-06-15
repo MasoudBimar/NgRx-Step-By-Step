56e63a05086d92deb5853ed08fd79f90d240172b
masoudbimar@gmail.com <sh.mirzaei@dolphinai.ir>
2026-06-15 13:23:28 +0200

    Update the repo with the latest changes

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected readonly title = signal('NgRx-Step-By-Step');
+
+  protected name = 'Peter';
+
+  /**
+   *
+   */
+  constructor() {
+
+    setTimeout(() => {
+      this.name = 'Marry';
+      console.log(this.name);
+    }, 5000);
+  }
+
+
+  doSth() {
+    console.log("sth")
+  }
+
+
 }
